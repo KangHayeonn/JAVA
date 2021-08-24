d5ef2bded933b226e4d34517575a1883b76ca0e4
KangHayeonn <rkdgkdus522@naver.com>
2021-08-24 17:56:36 +0900

    MapTest, Q1 package re-upload

---
+++ JavaStudy/src/Q1/Solution.java
@@ -0,0 +1,37 @@
+// 숫자 문자열과 영단어 (프로그래머스 LEVEL1)
+
+package Q1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		s.solution("one4seveneight");
+	}
+	
+    public int solution(String s) {
+        int answer = 0;
+        Map<String, Integer> map = new HashMap<>(); // 시간 복잡도 : O(1) , 키 값이 배열의 인덱스로 변환
+        map.put("zero", 0);
+    	map.put("one", 1);
+    	map.put("two", 2);
+    	map.put("three", 3);
+    	map.put("four", 4);
+    	map.put("five", 5);
+    	map.put("six", 6);
+    	map.put("seven", 7);
+    	map.put("eight", 8);
+    	map.put("nine", 9);
+    	
+    	for(String key : map.keySet()) {
+            System.out.println(key+":"+map.get(key));
+         }
+    	
+        System.out.println("안녕하세요");
+        return answer;
+    }
+
+}
