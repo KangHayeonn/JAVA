b047fd14d08a78997b5017d8f1276d5e12d48390
KangHayeonn <rkdgkdus522@naver.com>
2021-08-24 20:05:53 +0900

    숫자 문자열과 영단어 (프로그래머스 LEVEL1) : 성공
    * Map 인터페이스 이용
    * map 사용하기 위한 HashMap 클래스 사용
    * repalce 함수 사용 : 문자열 치환
    * parseInt 함수 사용 : Integer 클래스의 static 함수
    
    - 작성자 : 강하연
    - 작성일 : 2021-08-24

--- JavaStudy/src/Q1/Solution.java
+++ JavaStudy/src/Q1/Solution.java
@@ -9,28 +9,31 @@ public class Solution {
 
 	public static void main(String[] args) {
 		Solution s = new Solution();
-		s.solution("one4seveneight");
+		System.out.println(s.solution("2three45sixseven"));
 	}
 	
     public int solution(String s) {
         int answer = 0;
-        Map<String, Integer> map = new HashMap<>(); // 시간 복잡도 : O(1) , 키 값이 배열의 인덱스로 변환
-        map.put("zero", 0);
-    	map.put("one", 1);
-    	map.put("two", 2);
-    	map.put("three", 3);
-    	map.put("four", 4);
-    	map.put("five", 5);
-    	map.put("six", 6);
-    	map.put("seven", 7);
-    	map.put("eight", 8);
-    	map.put("nine", 9);
+        String newStr = "";
+        Map<String, String> map = new HashMap<>(); // 시간 복잡도 : O(1) , 키 값이 배열의 인덱스로 변환
+        map.put("zero", "0");
+    	map.put("one", "1");
+    	map.put("two", "2");
+    	map.put("three", "3");
+    	map.put("four", "4");
+    	map.put("five", "5");
+    	map.put("six", "6");
+    	map.put("seven", "7");
+    	map.put("eight", "8");
+    	map.put("nine", "9");
     	
     	for(String key : map.keySet()) {
-            System.out.println(key+":"+map.get(key));
-         }
-    	
-        System.out.println("안녕하세요");
+           if(s.contains(key)) {
+        	   newStr = s.replace(key, map.get(key));
+        	   s = newStr;
+           }
+    	}
+    	answer = Integer.parseInt(s);
         return answer;
     }
 
