03c75605e36ce7ba39dd79e608d556a57444c5d6
KangHayeonn <rkdgkdus522@naver.com>
2021-09-09 00:40:26 +0900

    크레인 인형뽑기 게임(프로그래머스 LEVEL1)
    : 성공 - ver2
    
    (별도의 테스트 케이스 추가하지 않음)
    
    * ver1의 코드에서 for-swich-for-if 였던 코드를 for-for-if 로 바꿔
      시간복잡도를 줄여 속도를 높였다.
    * 문제풀이 알고리즘은 ver1 과 동일
    
    - 작성자 : 강하연
    - 작성일 : 2021-09-09

--- JavaStudy/src/codingtest_9th_2nd/Q3.java
+++ JavaStudy/src/codingtest_9th_2nd/Q3.java
@@ -1,17 +1,10 @@
 // 크레인 인형뽑기 게임 (프로그래머스 LEVEL1)
 
-// 알고리즘
-// (1) moves 에서 주어진 요소 중에서 해당 board의 인덱스를 비교
-// (2) (1)번의 결과, 0이 아닌 값이 있으면 그 숫자를 새로운 배열에 저장
-// (3) (1)번의 결과, 0이면 continue
-// (4) 해당 배열을 모두 저장해준 뒤 비교함수로 이동
-// (5) 배열의 요소를 돌면서 자신의 앞의 숫자가 자신과 동일할 경우 삭제해주고 다시 비교함수로 재귀를 돌린다.
-// (6) 모든 원소 각 부분 앞에 동일한게 없으면 해당 재귀함수를 return
-// (7) 삭제해줄때는 본인과 자신의 앞을 카운트 해줘야 하기 때문에 answer +2 해준다.
-// (8) 최종적으로 삭제된 값인 answer을 출력
+// ver2 : 성공
+// solution 함수의 메인 for문 내의 switch 문을 삭제해 시간 복잡도를 줄임
+// 추가 테스트 케이스는 없음
+// 알고리즘 ver1과 동일
 
-// ver1 : 실패
-// 테스트 4,7,8,9,10,11 실패
 package codingtest_9th_2nd;
 
 import java.util.ArrayList;
@@ -19,12 +12,10 @@ import java.util.ArrayList;
 public class Q3 {
 	static public void main(String args[]) {
 		Q3 s = new Q3();
-		int[][] board = {{0,0,0,0,0},
-						{0,0,1,0,3},
-						{0,2,5,0,1},
-						{4,2,4,4,2},
-						{3,5,1,3,1}};
-		int[] moves = {1,5,3,5,1,2,1,4};
+		int[][] board = {{0,2,0},
+						{1,2,0},
+						{2,2,1}};
+		int[] moves = {1,2,2,3};
 		System.out.println(s.solution(board, moves));
 		return;
 	}
@@ -34,58 +25,14 @@ public class Q3 {
         int answer = 0;
    
         for(int i=0; i<moves.length; i++) {
-        	switch(moves[i]) {
-	        	case 1: 
-	        		for(int j=0; j< board.length; j++) {
-	        			if(board[j][0] != 0) {
-	        				arr.add(board[j][0]);
-	        				board[j][0] = 0;
-	        				break;
-	        			}
-	        			else continue;
-	        		}
-	        		break;
-	        	case 2:
-	        		for(int j=0; j< board.length; j++) {
-	        			if(board[j][1] != 0) {
-	        				arr.add(board[j][1]);
-	        				board[j][1] = 0;
-	        				break;
-	        			}
-	        			else continue;
-	        		}
-	        		break;
-	        	case 3:
-	        		for(int j=0; j< board.length; j++) {
-	        			if(board[j][2] != 0) {
-	        				arr.add(board[j][2]);
-	        				board[j][2] = 0;
-	        				break;
-	        			}
-	        			else continue;
-	        		}
-	        		break;
-	        	case 4:
-	        		for(int j=0; j< board.length; j++) {
-	        			if(board[j][3] != 0) {
-	        				arr.add(board[j][3]);
-	        				board[j][3] = 0;
-	        				break;
-	        			}
-	        			else continue;
-	        		}
-	        		break;
-	        	case 5:
-	        		for(int j=0; j< board.length; j++) {
-	        			if(board[j][4] != 0) {
-	        				arr.add(board[j][4]);
-	        				board[j][4] = 0;
-	        				break;
-	        			}
-	        			else continue;
-	        		}
-	        		break;
-        	}
+    		for(int j=0; j< board.length; j++) {
+    			if(board[j][moves[i]-1] != 0) {
+    				arr.add(board[j][moves[i]-1]);
+    				board[j][moves[i]-1] = 0;
+    				break;
+    			}
+    			else continue;
+    		}		
         }
         
         answer = comparison(arr, answer);
