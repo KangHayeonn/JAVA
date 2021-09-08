ed3875a470920fbf6db54621a59b33c5b0e82730
KangHayeonn <rkdgkdus522@naver.com>
2021-09-08 12:32:29 +0900

    체육복 (프로그래머스 LEVEL1) - ver3
    : 성공
    
    * ver2의 코드의 반복문과 조건문을 두가지로 분리
    * 테스트케이스 추가
    - 입력값 > 5, [2,3,4] , [3,4,5]
    - 기댓값 > 4
    
    - 작성자 : 강하연
    - 작성일 : 2021.09.08

--- JavaStudy/src/codingtest_9th_2nd/Q1.java
+++ JavaStudy/src/codingtest_9th_2nd/Q1.java
@@ -1,31 +1,56 @@
 // 체육복 (프로그래머스 LEVEL1)
 
-//ver 2 : 테스트케이스 12, 13 18 실패 (나머지는 모두 성공)
+// ver 3 : 테스트케이스 추가해서 수정
+// 입력값 > 5, [2,3,4] , [3,4,5]
+// 기댓값 > 4 
 package codingtest_9th_2nd;
+
+import java.util.Arrays;
+
 public class Q1 {
 	static public void main(String[] args) {
 		Q1 s = new Q1();
-		int lost[] = {1,4,5};
-		int reserve[] = {1,4,6};
+		int lost[] = {2,3,4};
+		int reserve[] = {1,2,3};
 		System.out.println(s.solution(5, lost, reserve));
 	}
 	
 	public int solution(int n, int[] lost, int[] reserve) {
 		int answer = 0;
 		int size = reserve.length-1;
+		int size2 = lost.length-1;
 		
 		answer = n - lost.length; // 안잃어버린 학생수
+		
+		Arrays.sort(lost);
+		Arrays.sort(reserve);
 		
 		for(int i=0; i<lost.length; i++) {
 			int key = lost[i];
 			for(int j=0; j<reserve.length; j++) {
-				if(reserve[j]==(key-1)||reserve[j]==(key)||reserve[j]==(key+1)) {
+				if(reserve[j]==(key)) {
+					answer++;
+					reserve = delete(j, reserve, size--);
+					lost = delete(i, lost, size2--);
+					i--;
+					break;
+				}
+				else continue;
+			}
+		}
+		
+		for(int i=0; i<lost.length; i++) {
+			int key = lost[i];
+			for(int j=0; j<reserve.length; j++) {
+				if(reserve[j]==(key-1)||reserve[j]==(key+1)) {
 					answer++;
 					reserve = delete(j, reserve, size--);
 					break;
 				}
+				else continue;
 			}
 		}
+        
         return answer;
 	}
 	
