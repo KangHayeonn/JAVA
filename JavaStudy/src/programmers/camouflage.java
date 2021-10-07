6c44368fd297c28f7ad58d1cfae674d49f6b8f8f
KangHayeonn <rkdgkdus522@naver.com>
2021-10-08 06:11:24 +0900

    위장 (프로그래머스 LEVEL2)
    : 성공
    
    * HashMap 사용
    * 이차원 배열을 출력할 때는 배열의 길이를 이용해서 출력
    * 이차원 배열 인수로 넘겨주는 법(실패) - 추후 연구 필요
    * lambda 값으로 직접 대입해서 넣어줄 때 문제 발생 -> 단순 : (forEach
      반복문)을 이용해서 해결
    
    - 작성자 : 강하연
    - 작성일 : 2021-10-08

---
+++ JavaStudy/src/programmers/camouflage.java
@@ -0,0 +1,52 @@
+// 위장 ( 프로그래머스 LEVEL2 )
+
+// 알고리즘
+// 각 clothes 원소의 경우 : clothes당 하나씩 입는 경우와 안입는경우
+// 즉, clothes의 원소의개수 + 1 (안입는경우)
+// 그래서 clothes의 종류 마다 위의 공식을 넣어 모두 곱해주면
+// 전체 다 안입는 경우가 하나 생긴다.
+// 이때, 스파이는 하루에 최소 한 개 이상은 입어야 하므로 전체에서 -1을 해준다.
+// clothes의 종류 key, 각각의 원소의 갯수 value (HashMap)
+
+package programmers;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class camouflage {
+	static public void main(String args[] {
+		camouflage s = new camouflage();
+		String[][] a = {
+				{"yellowhat", "headgear"}, 
+				{"bluesunglasses", "eyewear"}, 
+				{"green_turban", "headgear"}
+		};
+		System.out.println(s.solution(a)); // 이차원배열을 인수로 넘겨주는 방법 아직 미지수
+	})
+	
+	public int solution(Stirng[][] clothes) {
+		int answer = 1;
+        HashMap <String, Integer> hashmap = new HashMap<String, Integer>();
+        for(int i=0; i<clothes.length; i++){
+            if(hashmap.containsKey(clothes[i][1])){
+                int num = hashmap.get(clothes[i][1]);
+                hashmap.replace(clothes[i][1], num= num+1);
+            }
+            else {
+                hashmap.put(clothes[i][1], 1);
+            }
+        }
+        /*
+        hashmap.forEach((key, value) -> {
+            System.out.println(key + " : " + value);
+            //answer += value; // lambda-error
+            // (error) local variables referenced from a lambda expression must be final or effectively final. 
+        });*/
+
+        for(String key : hashmap.keySet()){
+            answer *= hashmap.get(key)+1;
+        }
+
+        return answer-1;
+	}
+}
