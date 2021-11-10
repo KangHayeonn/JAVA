d98445e217add2e2484844241937b8f70bb33b7d
KangHayeonn <rkdgkdus522@naver.com>
2021-11-10 14:53:58 +0900

    Graph 6가지 방식으로 구현
    : 성공
    
    * DFS : 배열/연결리스트 + 재귀/스택
    * BFS : 배열/연결리스트 + 큐
    * DFS와 BFS (백준 1260번)으로 테스트
    
    - 작성자 : 강하연
    - 작성일 : 2021-11-10

--- JavaStudy/src/baekjoon/Q1260.java
+++ JavaStudy/src/baekjoon/Q1260.java
@@ -1,16 +1,17 @@
-package Algorithm;
+// DFS와 BFS (백준 1260번)
+
+package baekjoon;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class Graph {
+public class Q1260 {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -20,7 +21,6 @@ public class Graph {
 		boolean[] isVisited = new boolean[N+1];
 		
 		// 인접행렬로 구현
-		/*
 		int[][] arr = new int[N+1][N+1];
 		
 		for(int i=0; i<M; i++) {
@@ -32,13 +32,13 @@ public class Graph {
 			arr[from][to] = 1;
 			arr[to][from] = 1; // 단방향일 경우 이부분 삭제
 		}
-		//DFS_Recursion_Array(arr, isVisited, N, M, start);
-		//DFS_Stack_Array(arr, isVisited, N, M, start);
+		DFS_Stack_Array(arr, isVisited, N, M, start);
+		System.out.println("");
+		isVisited = new boolean[N+1];
 		BFS_Queue_Array(arr, isVisited, N, M, start);
-		*/
 		
 		// 인접리스트로 구현
-		
+		/*
 		LinkedList<Integer>[] list = new LinkedList[N+1]; // 정점 수 만큼 배열 생성(연결리스트 갯수)
 		
 		for(int i=0; i<=N; i++) {
@@ -62,31 +62,12 @@ public class Graph {
 		DFS_Recursion_LL(list, isVisited, N, M, start);
 		System.out.println("");
 		isVisited = new boolean[N+1];
-		//DFS_Stack_LL(list, isVisited, N, M, start);
 		BFS_Queue_LL(list, isVisited, N, M, start);
-		
-		// 연결리스트 출력법
-		/*
-		for(int i=0; i<N+1; i++) {
-			System.out.print(i + " : ");
-			for(Integer j : list[i]) {
-				System.out.print(j+ " ");
-			}
-			System.out.println("");
-		}*/
-	}
-	
-	// DFS 구현 : 인접행렬 & 재귀
-	public static void DFS_Recursion_Array(int[][] arr, boolean[] isVisited, int N, int M, int V) {
-		isVisited[V] = true;
-		System.out.print(V + " ");
-		
-		for(int i=1; i<N+1; i++) {
-			if(arr[V][i] == 1 && !isVisited[i]) DFS_Recursion_Array(arr, isVisited, N, M, i);
-		}
+		*/
 	}
 	
 	// DFS 구현 : 인접리스트 & 재귀
+	
 	public static void DFS_Recursion_LL(LinkedList<Integer>[] list, boolean[] isVisited, int N, int M, int V) {
 		isVisited[V] = true;
 		System.out.print(V + " ");
@@ -110,32 +91,6 @@ public class Graph {
 			
 			for(int i=1; i<N+1; i++) {
 				if(arr[x][i] == 1 && !isVisited[i]) {
-					stack.push(i);
-					System.out.print(i + " ");
-					check = true;
-					isVisited[i] = true;
-					break; // 필수
-				}
-			}
-			
-			if(!check) stack.pop();
-		}
-	}
-	
-	// DFS 구현 : 인접리스트 & 스택
-	public static void DFS_Stack_LL(LinkedList<Integer>[] list, boolean[] isVisited, int N, int M, int V) {
-		Stack<Integer> stack = new Stack<Integer>();
-		boolean check;
-		isVisited[V] = true;
-		System.out.print(V + " ");
-		stack.push(V);
-		
-		while(!stack.isEmpty()) {
-			int x = stack.peek();
-			check = false;
-			
-			for(Integer i : list[x]) {
-				if(!isVisited[i]) {
 					stack.push(i);
 					System.out.print(i + " ");
 					check = true;
