b36c503c3d2aa442931e482565f059c73920b3c9
KangHayeonn <rkdgkdus522@naver.com>
2021-12-04 18:36:47 +0900

    괄호 제거 (백준 2800번)
    : 성공
    
    * 스택, ArrayList, Set(TreeSet), 조합 이용
    
    - 작성자 : 강하연
    - 작성일 : 2021-12-04

--- JavaStudy/src/baekjoon/Q1991.java
+++ JavaStudy/src/baekjoon/Q1991.java
@@ -8,82 +8,81 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-import baekjoon.Q1991.Tree.Node;
-
-public class Q1991 {
-	public static class Tree {
-		private int count;
+class Tree {
+	private static int count;
+	
+	public Tree() {
+		count =0;
+	}
+	public static class Node {
+		Object data;
+		Node left;
+		Node right;
 		
-		public Tree() {
-			count =0;
-		}
-		public class Node {
-			Object data;
-			Node left;
-			Node right;
-			
-			public Node(Object data) {
-				this.data =data;
-				left = null;
-				right = null;
-			}
-			
-			public void addLeft(Node node) {
-				left = node;
-				count++;
-			}
-			
-			public void addRight(Node node) {
-				right = node;
-				count++;
-			}
-			
-			public void deleteLeft() {
-				left = null;
-				count--;
-			}
-			
-			public void deleteRight() {
-				right = null;
-				count--;
-			}
+		public Node(Object data) {
+			this.data =data;
+			left = null;
+			right = null;
 		}
 		
-		public Node addNode(Object data) {
-			Node n = new Node(data);
-			return n;
+		public void addLeft(Node node) {
+			left = node;
+			count++;
 		}
 		
-		//전위 순회 (루트 -> 좌 -> 우)
-		public void preOrder(Node node) {
-			if(node==null) {
-				return;
-			}
-			System.out.print(node.data+" ");
-			preOrder(node.left);
-			preOrder(node.right);
+		public void addRight(Node node) {
+			right = node;
+			count++;
 		}
 		
-		//중위 순회 (좌 -> 루트 -> 우)
-		public void inOrder(Node node) {
-			if(node==null) {
-				return;
-			}
-			inOrder(node.left);
-			System.out.print(node.data+" ");
-			inOrder(node.right);
+		public void deleteLeft() {
+			left = null;
+			count--;
 		}
 		
-		//후위 순회 (좌 -> 우 -> 루트)
-		public void postOrder(Node node) {
-			if(node==null) {
-				return;
-			}
-			postOrder(node.left);
-			postOrder(node.right);
-			System.out.print(node.data+" ");
+		public void deleteRight() {
+			right = null;
+			count--;
 		}
 	}
+	
+	public Node addNode(Object data) {
+		Node n = new Node(data);
+		return n;
+	}
+	
+	//전위 순회 (루트 -> 좌 -> 우)
+	public void preOrder(Node node) {
+		if(node==null) {
+			return;
+		}
+		System.out.print(node.data+" ");
+		preOrder(node.left);
+		preOrder(node.right);
+	}
+	
+	//중위 순회 (좌 -> 루트 -> 우)
+	public void inOrder(Node node) {
+		if(node==null) {
+			return;
+		}
+		inOrder(node.left);
+		System.out.print(node.data+" ");
+		inOrder(node.right);
+	}
+	
+	//후위 순회 (좌 -> 우 -> 루트)
+	public void postOrder(Node node) {
+		if(node==null) {
+			return;
+		}
+		postOrder(node.left);
+		postOrder(node.right);
+		System.out.print(node.data+" ");
+	}
+}
+
+public class Q1991 {
 	
 	public static void main(String args[]) throws IOException {
 		
@@ -98,7 +97,7 @@ public class Q1991 {
 			st = new StringTokenizer(br.readLine());
 			for(int j=0; j<3; j++) {
 				String str = st.nextToken();
-				Q1991.Tree.Node n = new Node('A');
+				Tree.Node n = tree.addNode('A');
 				if(str.charAt(0) != '.' ) { // 대문자 알파벳일 때
 					if(j==0) {
 						if(str.charAt(0) != 'A') {
@@ -106,10 +105,10 @@ public class Q1991 {
 						}
 					}
 					else if(j==1) {
-						n.addLeft(new Node(str.charAt(0)));
+						n.addLeft(new Tree.Node(str.charAt(0)));
 					}
 					else {
-						n.addRight(new Node(str.charAt(0)));
+						n.addRight(new Tree.Node(str.charAt(0)));
 					}
 					
 				} else { //. 일 때
@@ -118,16 +117,16 @@ public class Q1991 {
 			}
 		}
 		
-		tree.preOrder(new Node('A'));
+		tree.preOrder(new Tree.Node('A'));
 		System.out.println();
-		tree.inOrder(new Node('A'));
+		tree.inOrder(new Tree.Node('A'));
 		System.out.println();
-		tree.postOrder(new Node('A'));
+		tree.postOrder(new Tree.Node('A'));
 		System.out.println();
 	}
-}
-*/
+}*/
 
+/*
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -208,4 +207,4 @@ public class Q1991 {
 		System.out.println();
 		tree.postOrder(tree.root);
 	}
-}
+}*/
