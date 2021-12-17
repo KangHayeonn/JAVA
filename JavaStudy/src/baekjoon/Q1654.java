ed5b6fb3b3f4d57b7630f3de09ec6fa1f76abff2
KangHayeonn <rkdgkdus522@naver.com>
2021-12-17 17:51:55 +0900

    랜선자르기 version 2 (min < max 버전) : 성공

--- JavaStudy/src/baekjoon/Q1654.java
+++ JavaStudy/src/baekjoon/Q1654.java
@@ -1,13 +1,12 @@
 // 랜선 자르기 (백준 1654번)
 
-// 내림차순으로 정렬
 // 제일 큰값과 제일 작은 값을 기준으로 이분탐색 -> mid : (제일 큰값 + 제일 작은 값) / 2
 // 만약 해당 값을 나머지 원소들에 나눴을 때 몫의 합이 N보다 작으면 최댓값을 줄여줌
 // 만약 해당 값을 나머지 원소들에 나눴을 때 몫의 합이 N보다 클 경우 최솟값을 늘려줌
 // 만약 N값과 같아지는 경우가 생기면 그 때의 해당 값을 출력
 
 package baekjoon;
-
+/*
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -32,12 +31,12 @@ public class Q1654 {
 		long mid = 0;
 		int count = 0;
 		
-		/* 경우의 수 
+		* 경우의 수 
 		 * 
 		 * case 1 : while(min <= max)  ->  else min = mid +1   -> 출력 max   (정답)
 		 * case 2 : while(min < max)   ->  else min = mid      -> 출력 min   (실패)
 		 * case 3 : while(min < max)   ->  else min = mid+1    -> 출력 min-1 (실패)
-		 */
+		 *
 		
 		while(min <= max) {   // 왜 < 이건 틀리고 <= 이건 맞는 걸까? (해당 조건 주의)
 			mid = (min+max)/2;
@@ -54,9 +53,10 @@ public class Q1654 {
 		}
 		System.out.print(max);
 		//System.out.println(Arrays.toString(arr));
+		
 	}
 	
-	/* 잘못된 코드
+	 잘못된 코드
 	public static int cutting(Integer[] arr, int K, int N) {
 		int left = 0;
 		int right = arr[0];
@@ -86,5 +86,52 @@ public class Q1654 {
 		}
 		
 		return mid; 
-	}*/
+	}
+}*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q1654 {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int K = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		Integer[] arr = new Integer[K];
+		
+		long max = 1;
+		
+		for(int i=0; i<K; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			if(arr[i] > max ) max = arr[i];
+		}
+		
+		max++; // 나눗셈에서 뭉개질 수 있으므로
+		
+		long min = 1;
+		long mid = 0;
+		int count = 0;
+        long answer = 1;
+		
+		while(min < max) {
+			mid = (min+max)/2;
+			
+			count = 0;
+			
+			for(int i=0; i<K; i++) {
+				count += arr[i]/mid;
+			}
+			
+			if(count < N) max = mid;
+			else {
+                min = mid+1;
+                answer = mid;
+            }
+		}
+		System.out.print(answer);
+	}
 }
+
