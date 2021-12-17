5d90b095446aac6cc9258036327397e3d44a13ba
KangHayeonn <rkdgkdus522@naver.com>
2021-12-17 16:25:17 +0900

    랜선 자르기 (백준 1654번)
    : 성공
    
    * 이분 탐색 (조건 주의)
    
    - 작성자 : 강하연
    - 작성일 : 2021-12-17

--- JavaStudy/src/baekjoon/Q1654.java
+++ JavaStudy/src/baekjoon/Q1654.java
@@ -1,9 +1,9 @@
 // 랜선 자르기 (백준 1654번)
 
 // 내림차순으로 정렬
-// 제일 큰값을 이분탐색 -> mid : 해당 값
-// 만약 해당 값을 나머지 원소들에 나눴을 때 몫의 합이 N보다 작으면 한번더 이분탐색
-// 만약 해당 값을 나머지 원소들에 나눴을 때 몫의 합이 N보다 클 경우 해당 값을 +1
+// 제일 큰값과 제일 작은 값을 기준으로 이분탐색 -> mid : (제일 큰값 + 제일 작은 값) / 2
+// 만약 해당 값을 나머지 원소들에 나눴을 때 몫의 합이 N보다 작으면 최댓값을 줄여줌
+// 만약 해당 값을 나머지 원소들에 나눴을 때 몫의 합이 N보다 클 경우 최솟값을 늘려줌
 // 만약 N값과 같아지는 경우가 생기면 그 때의 해당 값을 출력
 
 package baekjoon;
@@ -11,7 +11,6 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Q1654 {
@@ -22,18 +21,42 @@ public class Q1654 {
 		int N = Integer.parseInt(st.nextToken());
 		Integer[] arr = new Integer[K];
 		
+		long max = 1;
+		
 		for(int i=0; i<K; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
+			if(arr[i] > max ) max = arr[i];
 		}
 		
-		Arrays.sort(arr, (a,b) -> b-a); // 내림차순 (Comparator로 비교하고 싶을 땐 int 대신 Integer 사용해야함)
+		long min = 1;
+		long mid = 0;
+		int count = 0;
 		
-		int answer = cutting(arr, K, N);
+		/* 경우의 수 
+		 * 
+		 * case 1 : while(min <= max)  ->  else min = mid +1   -> 출력 max   (정답)
+		 * case 2 : while(min < max)   ->  else min = mid      -> 출력 min   (실패)
+		 * case 3 : while(min < max)   ->  else min = mid+1    -> 출력 min-1 (실패)
+		 */
 		
-		System.out.print(answer);
+		while(min <= max) {   // 왜 < 이건 틀리고 <= 이건 맞는 걸까? (해당 조건 주의)
+			mid = (min+max)/2;
+			
+			count = 0;
+			
+			for(int i=0; i<K; i++) {
+				count += arr[i]/mid;
+			}
+			
+			if(count < N) max = mid-1;
+			else min = mid+1;
+			
+		}
+		System.out.print(max);
 		//System.out.println(Arrays.toString(arr));
 	}
 	
+	/* 잘못된 코드
 	public static int cutting(Integer[] arr, int K, int N) {
 		int left = 0;
 		int right = arr[0];
@@ -63,5 +86,5 @@ public class Q1654 {
 		}
 		
 		return mid; 
-	}
+	}*/
 }
