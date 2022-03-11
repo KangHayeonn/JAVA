87b3a9bc0be5c3342150f4b987e5a4245fd8d178
yeoni sugar <rkdgkdus522@naver.com>
2022-03-11 17:41:52 +0900

    [1차] 다트 게임 (프로그래머스 LEVEL1 - Kakao)
    : 성공
    
    * ArrayList, Stack 사용
    
    - 작성자 : 강하연
    - 작성일 : 2022-03-11

--- JavaStudy/src/programmers/DartGame.java
+++ JavaStudy/src/programmers/DartGame.java
@@ -1,5 +1,74 @@
+// [1차] 다트 게임 (프로그래머스 LEVEL1 - Kakao)
+
 package programmers;
 
-public class DartGame {
+import java.util.ArrayList;
+import java.util.Stack;
 
+public class DartGame {
+	public static void main(String args[]) {
+		String s = "1S2D*3T";
+		System.out.println(solution(s));
+	}
+	public static int solution(String dartResult) {
+		int answer = 0;
+		int num = 0;
+		ArrayList<Integer> arr = new ArrayList<>();
+		Stack<Integer> stack = new Stack<>();
+		
+		for(int i=0; i<dartResult.length(); i++) {
+			char c = dartResult.charAt(i);
+			
+			if(Character.isDigit(c) ) {
+				if(num==0) {
+					num = c - '0'; // 숫자로 변환 (0~9)
+				} else { // num =1 일때
+					num = num*10; // 숫자 10 이 들어올 경우
+				}
+			}
+			
+			else {
+				switch(c) {
+					case 'S': num= num*1; arr.add(num); break;
+					case 'D': num= num*num; arr.add(num); break;
+					case 'T': num= num*num*num; arr.add(num); break;
+					case '*': arr.add(-1); break;
+					case '#': arr.add(-2); break;
+					default: break;
+				}
+				num=0;
+			}
+		}
+		
+		for(int i: arr) {
+			if(i==-1 || i==-2) { // -1: *, -2: #
+				int n1 = 0;
+				int n2 = 0;
+				
+				if(i==-1) {
+					if(stack.size()==1) {
+						n1 = stack.pop() * 2;
+						stack.add(n1);
+					} else {
+						n1 = stack.pop() * 2;
+						n2 = stack.pop() * 2;
+						stack.add(n2);
+						stack.add(n1);
+					}
+				} else {
+					n1 = stack.pop() * (-1);
+					stack.add(n1);
+				}
+			} else {
+				stack.add(i);
+			}
+		}
+		
+		while(!stack.isEmpty()) {
+			int a = stack.pop();
+			answer += a;
+		}
+
+		return answer;
+	}
 }
--- JavaStudy/src/programmers/StringCompression.java
+++ JavaStudy/src/programmers/StringCompression.java
@@ -1,5 +1,68 @@
+// 문자열 압축 (프로그래머스 LEVEL2)
+
 package programmers;
 
-public class StringCompression {
+import java.util.ArrayList;
+import java.util.Collections;
 
+public class StringCompression {
+	public static void main(String args[]) {
+		String s = "aabbaccc";
+		System.out.println(solution(s));
+	}
+	public static int solution(String s) {
+		int answer=0;
+        int i, end;
+        String str;
+		ArrayList<String> arr;
+		ArrayList<Integer> str_length = new ArrayList<Integer>();
+		if(s.length()==1) return 1;
+		for(int cnt=1; cnt<s.length()/2+1; cnt++) {
+			i=0;
+			arr = new ArrayList<String>();
+			while(i < s.length()) { 
+				end = i+cnt;
+				if(end > s.length()) end = s.length();
+				str = s.substring(i,end); 
+				i=end;
+				arr.add(str);  
+			}
+			str_length.add(comparison(arr)); 
+		}
+		answer = Collections.min(str_length);
+		return answer;
+	}
+	public static int comparison(ArrayList<String> arr) {
+		String result = "";
+		String a,b;
+		int count=1;
+		for(int i=0; i<arr.size()-1; i++) {
+			a= arr.get(i);
+			b= arr.get(i+1);
+            
+			if(a.equals(b)) { // ==이걸로 문자열 비교 안됨
+				count++;
+			}
+			else {
+				if(count==1) {
+					result += arr.get(i);
+				}
+				else {
+					result += count+arr.get(i); 
+					count=1;
+				}
+			}
+            
+			if(i==arr.size()-2) {
+				if(count==1) {
+					result += arr.get(i+1);
+				}
+				else {
+					result += count+arr.get(i+1);
+					count=1;
+				}
+			}
+        }
+		return result.length();
+	}
 }
