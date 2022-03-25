9b8ec1cb121ffdd6d250233c2a08d56fa417c81e
yeoni sugar <rkdgkdus522@naver.com>
2022-03-26 00:52:08 +0900

    거리두기 확인하기 (프로그래머스 LEVEL2 - Kakao)
    : 성공
    
    * BFS - 알고리즘 조건 설정이 중요

--- JavaStudy/src/TestPractice/CheckDistance.java
+++ JavaStudy/src/TestPractice/CheckDistance.java
@@ -3,53 +3,61 @@
 package TestPractice;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class CheckDistance {
-	private static int[][] map;
-	private static boolean[][] visited;
-    private static int[] dx = {0, 0, -1, 1};
-    private static int[] dy = {-1, 1, 0, 0};
-    private static int M, N;
-    
-    static class type {
-        int x;
-        int y;
-        
-        public type(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-    
 	public static void main(String args[]) {
 		String[][] places = {{"POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"}, {"POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"}, {"PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"}, {"OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"}, {"PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"}};
-		System.out.println(solution(places));
+		System.out.println(Arrays.toString(solution(places)));
 	}
 	public static int[] solution(String[][] places) {
-		int[] answer = {};
-		M = places[0].length; // x축 길이
-        N = places.length; // y축 길이
+		int[] answer = new int[places.length];
         
-        for(int i=0; i<N; i++) {
-        	map= new int[5][5];
-        	visited = new boolean[5][5];
-        	for(int j=0; j<M; j++) {
+        for(int i=0; i<places.length; i++) {
+        	String[] p = places[i];
+            boolean check = true;
+        	
+        	for(int j=0; j<5; j++) {
         		for(int k=0; k<5; k++) {
-        			char s = places[i][j].charAt(k);
+        			char s = p[j].charAt(k);
         			if(s=='P') {
-        				map[j][k] = 1;
-        			} else if(s=='O') {
-        				map[j][k] = 0;
-        			} else {
-        				map[j][k] = -1;
+        				if(!bfs(p, j, k)) check = false;
         			}
         		}
-        		System.out.println(places[i][j]);
         	}
-        	System.out.println(Arrays.deepToString(map));
+        	
+        	answer[i] = check ? 1: 0;
         }
         return answer;
-        
+	}
+	public static boolean bfs(String[] p, int r, int c) { // row: 가로, column: 세로
+		int[] dx = {0, 0, -1, 1};
+	    int[] dy = {-1, 1, 0, 0};
+		
+		Queue<int[]> q = new LinkedList<>();
+		q.add(new int[] {r, c});
+		
+		while(!q.isEmpty()) {
+			int[] pos = q.poll();
+			
+			for(int i=0; i<4; i++) {
+				int nr = pos[0] + dx[i]; // 가로
+				int nc = pos[1] + dy[i];    // 세로
+                
+                // 처음 출발점 P는 제외 (중요!)
+				if(nr == r && nc == c) continue;
+				
+				if(nr<0 || nr>=5 || nc<0 || nc>=5) continue;
+				
+				// 맨헤튼거리 계산
+				int d = Math.abs(nr-r) + Math.abs(nc-c);
+				
+				if(d<=2 && p[nr].charAt(nc) == 'P') return false;
+				else if(d<2 && p[nr].charAt(nc) == 'O') q.add(new int[] {nr, nc});
+			}
+		}
+		return true;
 	}
 }
 
