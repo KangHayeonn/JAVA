2786899a43a7346e6efc50dafa442789399e727b
yeoni sugar <rkdgkdus522@naver.com>
2022-03-26 23:05:25 +0900

    숫자 게임 (프로그래머스 LEVEL2 - Kakao)
    : 성공
    
    * 정렬, 완전탐색
    
    - 작성자 : 강하연
    - 작성일 : 2022-03-26

---
+++ JavaStudy/src/programmersSchool/NumberGame.java
@@ -0,0 +1,37 @@
+// 숫자 게임 (프로그래머스 LEVEL2 - Kakao)
+
+// 정렬, 완전탐색
+
+package programmersSchool;
+
+import java.util.Arrays;
+
+public class NumberGame {
+	public static void main(String args[]) {
+		int[] A = {5, 1, 3, 7};
+		int[] B = {2, 2, 6, 8};
+		
+		System.out.println(solution(A, B));
+	}
+	public static int solution(int[] A, int[] B) {
+		int answer = 0;
+		Arrays.sort(A);
+		Arrays.sort(B);
+		
+		int idx = 0;
+		
+		for(int i=0; i<A.length; i++) {
+			for(int j=idx; j<B.length;) {
+				if(A[i]<B[j]) {
+					answer++;
+					idx = j+1;
+					break;
+				} else {
+					j++;
+				}
+			}
+		}
+		
+		return answer;
+	}
+}
