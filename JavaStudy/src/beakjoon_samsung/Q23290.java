4f286b8edf00202e789de24aa3dee6b2db5ee9fe
KangHayeonn <rkdgkdus522@naver.com>
2022-10-12 17:44:29 +0900

    삼성 역량테스트 문제풀이 - 마법사 상어와 복제
    : 성공
    
    * 마법사 상어와 복제 (백준 골드1)
    * 디버깅 매우 어려움
    * 세세한 조건 체크 잘하기
    * 깊은 복사 개념 중요
    * 중복 순열, 백트래킹
    * 우선순위큐, 큐, bfs
    
    - 작성자 : 강하연
    - 작성일 : 2022-10-12

--- JavaStudy/src/beakjoon_samsung/Q23290.java
+++ JavaStudy/src/beakjoon_samsung/Q23290.java
@@ -7,56 +7,67 @@ import java.util.*;
 
 public class Q23290 {
 	static int M, S;
-	static MapType[][] map = new MapType[4][4];
-	static SmellType[][] smellChkMap = new SmellType[4][4];
-	static int fishIdx = 1;
-	static Map<Integer, Type> fishMap;
-	static int shark_r, shark_c;
+	static int shark_r, shark_c; // 상어의 위치
+	static Type[][] infoMap;
 	static int[] dr = {0, -1, -1, -1, 0, 1, 1, 1};
 	static int[] dc = {-1, -1, 0, 1, 1, 1, 0, -1};
-	static int[] shark_dr = {-1, 0, 1, 0}; // 상 좌 하 우
-	static int[] shark_dc = {0, -1, 0, 1};
-	static ArrayList<String> sharkMoveDict;
-	public static class Type {
-		int r, c;
-		int d;
-		public Type(int r, int c, int d) {
+	static int[] drS = {-1, 0, 1, 0}; // 상좌하우 (상어) <- (디버깅0) **
+	static int[] dcS = {0, -1, 0, 1};
+	static Type[][] tmpInfoMap; // 복제 마법 저장
+	static ArrayList<Cood> tmpArr; // 임시 배열
+	static Map<Integer, Integer> tmpMap; // 임시 맵
+	static ArrayList<Type2> sharkMoveList;
+	static int[][] smellMap;
+	public static class Type { // 물고기의 정보를 담은 map을 위한 type
+		int cnt; // 물고기의 수
+		Map<Integer, Integer> fishMap; // key : 방향, value : 물고기 개수 (동일한 방향의 물고기는 하나로 묶임)
+		public Type(int cnt, Map<Integer, Integer> fishMap) {
+			this.cnt = cnt;
+			this.fishMap = fishMap;
+		}
+	}
+	public static class Type2 {
+		int first;
+		int second;
+		int third;
+		public Type2(int first, int second, int third) {
+			this.first = first;
+			this.second = second;
+			this.third = third;
+		}
+	}
+	public static class Cood {
+		int r, c, d;
+		public Cood(int r, int c, int d) {
 			this.r = r;
 			this.c = c;
 			this.d = d;
 		}
 	}
-	public static class MapType {
-		int cnt;
-		ArrayList<Integer> arr;
-		public MapType(int cnt, ArrayList<Integer> arr) {
+	public static class Type3 {
+		int r, c;
+		int cnt; // 물고기의 수
+		Map<Integer, Integer> fishMap; // key : 방향, value : 물고기 개수 (동일한 방향의 물고기는 하나로 묶임)
+		public Type3(int r, int c, int cnt, Map<Integer, Integer> fishMap) {
+			this.r = r;
+			this.c = c;
 			this.cnt = cnt;
-			this.arr = arr;
+			this.fishMap = fishMap;
 		}
 	}
-	public static class SmellType {
-		boolean chk; // true: 물고기 냄새 유, false: 물고기 냄새 무
-		int cnt; // 몇번 지워졌는지
-		public SmellType(boolean chk, int cnt) {
-			this.chk = chk;
-			this.cnt = cnt;
-		}
-	}
-	public static class SharkType { 
-		int rFirst, cFirst;
-		int rSecond, cSecond;
-		int rThird, cThird;
-		int cnt;
+	public static class PQType { 
+		int sum;
 		String s;
-		public SharkType(int rFirst, int cFirst, int rSecond, int cSecond, int rThird, int cThird, int cnt, String s) {
-			this.rFirst = rFirst;
-			this.cFirst = cFirst;
-			this.rSecond = rSecond;
-			this.cSecond = cSecond;
-			this.rThird = rThird;
-			this.cThird = cThird;
-			this.cnt = cnt;
+		int nr1, nr2, nr3, nc1, nc2, nc3;
+		public PQType(int sum, String s, int nr1, int nr2, int nr3, int nc1, int nc2, int nc3) {
+			this.sum = sum;
 			this.s = s;
+			this.nr1 = nr1;
+			this.nr2 = nr2;
+			this.nr3 = nr3;
+			this.nc1 = nc1;
+			this.nc2 = nc2;
+			this.nc3 = nc3;
 		}
 	}
 	public static void main(String args[]) throws IOException {
@@ -65,261 +76,243 @@ public class Q23290 {
 		M = Integer.parseInt(st.nextToken());
 		S = Integer.parseInt(st.nextToken());
 		
-		fishMap = new HashMap<>();
-		int r, c, d;
-		ArrayList<Integer> tmpArr;
+		infoMap = new Type[4][4];
+		smellMap = new int[4][4];
+		
+		for(int i=0; i<4; i++) {
+			for(int j=0; j<4; j++) {
+				tmpMap = new HashMap<>();
+				infoMap[i][j] = new Type(0, tmpMap);
+			}
+		}
+		
+		int fx, fy, d;
 		for(int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
-			r = Integer.parseInt(st.nextToken())-1;
-			c = Integer.parseInt(st.nextToken())-1;
-			d = Integer.parseInt(st.nextToken())-1;
-			fishMap.put(fishIdx, new Type(r, c, d));
+			fx = Integer.parseInt(st.nextToken()) - 1;
+			fy = Integer.parseInt(st.nextToken()) - 1;
+			d = Integer.parseInt(st.nextToken()) - 1;
 			
-			if(map[r][c] == null) {
-				tmpArr = new ArrayList<Integer>();
-				tmpArr.add(fishIdx);
-				map[r][c] = new MapType(1, tmpArr);
-			} else {
-				MapType t = map[r][c];
-				t.cnt += 1;
-				t.arr.add(fishIdx);
-				map[r][c] = t; 
-			}
-			fishIdx += 1;
+			Type t = infoMap[fx][fy];
+			t.fishMap.put(d,  t.fishMap.getOrDefault(d, 0) + 1); // map 초기화 입력 잘못 받음 (디버깅1) **
+			t.cnt += 1;
+			
+			infoMap[fx][fy] = t;
 		}
 		
 		st = new StringTokenizer(br.readLine());
-		shark_r = Integer.parseInt(st.nextToken())-1;
-		shark_c = Integer.parseInt(st.nextToken())-1;
+		shark_r = Integer.parseInt(st.nextToken()) - 1; // 상어 좌표 잘못 받음 (디버깅2) **
+		shark_c = Integer.parseInt(st.nextToken()) - 1;
 		
+		sharkMoveList = new ArrayList<>();
 		int[] arr = {0, 1, 2, 3};
-		int[] pickArr = new int[3];
-		sharkMoveDict = new ArrayList<>();
+		int[] tempArr = new int[3];
+		permutation(arr, tempArr, 0);
 		
-		getSharkMoveDict(arr, pickArr, 0);
-		
-		Queue<Type> tmpQ;
 		while(S-- > 0) {
-			// 상어가 모든 물고기에게 복제 마법을 시전 (1번)
-			tmpQ = new LinkedList<>();
-			for(Integer key : fishMap.keySet()) {
-				Type t = fishMap.get(key);
-				tmpQ.offer(new Type(t.r, t.c, t.d));
+			// (1번) 복제마법
+			tmpInfoMap = new Type[4][4];
+			for(int i=0; i<4; i++) {
+				for(int j=0; j<4; j++) {
+					// tmpInfoMap[i][j] = infoMap[i][j]; <- 깊은복사 안됨(얕은복사됨) // 디버깅 이슈***
+					Type t = infoMap[i][j];
+					tmpMap = new HashMap<>();
+					
+					// 깊은 복사 (Map, ArrayList등등) *** (디버깅 핵심 부분!!!)
+					for(Map.Entry<Integer, Integer> entry : t.fishMap.entrySet()) {
+						tmpMap.put(entry.getKey(), entry.getValue());
+					}
+					tmpInfoMap[i][j] = new Type(t.cnt, tmpMap);
+				}
 			}
 			
-			// 모든 물고기가 한칸 이동 (2번)
+			// (2번) 모든 물고기가 한 칸 이동
 			moveFish();
-
-			// 상어가 연속해서 3칸 이동 (3번)
+			
+			// (3번) 상어가 연속해서 3칸
 			moveShark();
 			
-			// 두 번 전 연습에서 생긴 물고기의 냄새가 격자에서 사라짐 (4번)
+			// (4번) 두 번 전 연습에서 생긴 물고기의 냄새가 격자에서 사라짐
 			deleteSmell();
 			
-			// 복제 마법 완료됨 (5번)
-			while(!tmpQ.isEmpty()) {
-				Type t = tmpQ.poll();
-				
-				fishMap.put(fishIdx, new Type(t.r, t.c, t.d));
-				
-				if(map[t.r][t.c] == null) {
-					tmpArr = new ArrayList<Integer>();
-					tmpArr.add(fishIdx);
-					map[t.r][t.c] = new MapType(1, tmpArr);
-				} else {
-					MapType m = map[t.r][t.c];
-					m.cnt += 1;
-					m.arr.add(fishIdx);
-					map[t.r][t.c] = m; 
-				}
-				fishIdx += 1;
-			}
+			// (5번) 1에서 사용한 복제 마법 완료됨
+			completeCopy();
 		}
 		
-		int sumFish = 0;
+		int result = 0;
 		for(int i=0; i<4; i++) {
 			for(int j=0; j<4; j++) {
-				if(map[i][j] != null) {
-					sumFish += map[i][j].cnt;
-				}
+				result += infoMap[i][j].cnt;
 			}
 		}
 		
-		System.out.println(sumFish);
+		System.out.println(result);
 	}
-	public static void moveFish() {
-		int nr=0, nc=0, nd=0;
-		boolean moveChk;
-		ArrayList<Integer> tmpArr;
-		for(Integer key : fishMap.keySet()) {
-			Type t = fishMap.get(key);
-			
-			moveChk = false;
-			for(int i=0; i<8; i++) {
-				nd = (t.d + -i + 8) % 8;
-				nr = t.r + dr[nd];
-				nc = t.c + dc[nd];
-				
-				if(nr < 0 || nc < 0 || nr >= 4 || nc >= 4) continue;
-				
-				if(nr == shark_r && nc == shark_c) continue;
-				
-				if(smellChkMap[nr][nc] != null && smellChkMap[nr][nc].chk) continue;
-				
-				moveChk = true;
-				break;
-			}
-			
-			if(moveChk) { // 이동
-				MapType m = map[t.r][t.c];
-				m.cnt -= 1;
-				m.arr.remove(key);
-				map[t.r][t.c] = m;
-				
-				if(map[nr][nc] == null) {
-					tmpArr = new ArrayList<Integer>();
-					tmpArr.add(key);
-					map[nr][nc] = new MapType(1, tmpArr);
-				} else {
-					MapType a = map[nr][nc];
-					a.cnt += 1;
-					a.arr.add(key);
-					map[nr][nc] = a; 
-				}
-				
-				fishMap.put(key, new Type(nr, nc, nd));
-			}
-		}
-	}
-	public static void getSharkMoveDict(int[] arr, int[] pickArr, int depth) {
-		if(depth == 3) {
-			String s = "";
-			for(int i=0; i<3; i++) {
-				s += pickArr[i];
-			}
-			sharkMoveDict.add(s);
-			return;
-		}
-		
-		// 순열
+	public static void completeCopy() {
 		for(int i=0; i<4; i++) {
-			pickArr[depth] = arr[i];
-			getSharkMoveDict(arr, pickArr, depth+1);
+			for(int j=0; j<4; j++) {
+				Type t = infoMap[i][j];
+				Type copyT = tmpInfoMap[i][j];
+				
+				t.cnt += copyT.cnt;
+				for(Integer key : copyT.fishMap.keySet()) {
+					t.fishMap.put(key, t.fishMap.getOrDefault(key, 0) + copyT.fishMap.get(key));
+				}
+			}
 		}
-	}
-	public static void moveShark() {
-		PriorityQueue<SharkType> pq = new PriorityQueue<>((a, b) -> {
-			if(a.cnt != b.cnt) return b.cnt - a.cnt;
-			else {
-				return (a.s).compareTo(b.s);
-			}
-		});
-		int first, second, third;
-		int nrFirst, ncFirst, nrSecond, ncSecond, nrThird, ncThird;
-		boolean[][] isVisited;
-		
-		int sum = 0;
-		for(String s : sharkMoveDict) {
-			isVisited = new boolean[4][4];
-			first = s.charAt(0) - '0';
-			second = s.charAt(1) - '0';
-			third = s.charAt(2) - '0';
-			
-			sum = 0;
-			
-			nrFirst = shark_r + shark_dr[first];
-			ncFirst = shark_c + shark_dc[first];
-			
-			if(nrFirst < 0 || ncFirst < 0 || nrFirst >= 4 || ncFirst >= 4) continue;
-			if(map[nrFirst][ncFirst] != null && !isVisited[nrFirst][ncFirst]) {
-				sum += map[nrFirst][ncFirst].cnt;
-			}
-			isVisited[nrFirst][ncFirst] = true;
-			
-			nrSecond = nrFirst + shark_dr[second];
-			ncSecond = ncFirst + shark_dc[second];
-			
-			if(nrSecond < 0 || ncSecond < 0 || nrSecond >= 4 || ncSecond >= 4) continue;
-			if(map[nrSecond][ncSecond] != null && !isVisited[nrSecond][ncSecond]) {
-				sum += map[nrSecond][ncSecond].cnt;
-			}
-			isVisited[nrSecond][ncSecond] = true;
-			
-			nrThird = nrSecond + shark_dr[third];
-			ncThird = ncSecond + shark_dc[third];
-			
-			if(nrThird < 0 || ncThird < 0 || nrThird >= 4 || ncThird >= 4) continue;
-			if(map[nrThird][ncThird] != null && !isVisited[nrThird][ncThird]) {
-				sum += map[nrThird][ncThird].cnt;
-			}
-			
-			pq.offer(new SharkType(nrFirst, ncFirst, nrSecond, ncSecond, nrThird, ncThird, sum, s));
-		}
-		
-		SharkType s = pq.poll();
-		
-		// 첫번째 이동에서 삭제
-		if(map[s.rFirst][s.cFirst] != null && map[s.rFirst][s.cFirst].cnt > 0) {
-			// map에서 삭제
-			MapType m = map[s.rFirst][s.cFirst];
-			for(Integer i : m.arr) {
-				fishMap.remove(i); // fishMap에서 삭제
-			}
-			m.cnt = 0;
-			m.arr.clear();
-			map[s.rFirst][s.cFirst] = m;
-			
-			// smellMap에 추가
-			smellChkMap[s.rFirst][s.cFirst] = new SmellType(true, 3);
-		}
-		
-		// 두번째 이동에서 삭제
-		if(map[s.rSecond][s.cSecond] != null && map[s.rSecond][s.cSecond].cnt > 0) {
-			// map에서 삭제
-			MapType m = map[s.rSecond][s.cSecond];
-			for(Integer i : m.arr) {
-				fishMap.remove(i); // fishMap에서 삭제
-			}
-			m.cnt = 0;
-			m.arr.clear();
-			map[s.rSecond][s.cSecond] = m;
-			
-			// smellMap에 추가
-			smellChkMap[s.rSecond][s.cSecond] = new SmellType(true, 3);
-		}
-		
-		// 세번째 이동에서 삭제
-		if(map[s.rThird][s.cThird] != null && map[s.rThird][s.cThird].cnt > 0) {
-			// map에서 삭제
-			MapType m = map[s.rThird][s.cThird];
-			for(Integer i : m.arr) {
-				fishMap.remove(i); // fishMap에서 삭제
-			}
-			m.cnt = 0;
-			m.arr.clear();
-			map[s.rThird][s.cThird] = m;
-			
-			// smellMap에 추가
-			smellChkMap[s.rThird][s.cThird] = new SmellType(true, 3);
-		}
-		
-		shark_r = s.rThird;
-		shark_c = s.cThird;
 	}
 	public static void deleteSmell() {
 		for(int i=0; i<4; i++) {
 			for(int j=0; j<4; j++) {
-				if(smellChkMap[i][j] != null) {
-					SmellType t = smellChkMap[i][j];
-					t.cnt -= 1;
+				if(smellMap[i][j] > 0) smellMap[i][j] -= 1;
+			}
+		}
+	}
+	public static void moveShark() {
+		PriorityQueue<PQType> pq = new PriorityQueue<>((a, b) -> {
+			if(a.sum != b.sum) return b.sum - a.sum;
+			else return (a.s).compareTo(b.s);
+		});
+		
+		int nr1, nr2, nr3, nc1, nc2, nc3;
+		int sum;
+		String s;
+		boolean[][] isVisited;
+		
+		for(Type2 t : sharkMoveList) { // first: 첫번째 이동 (...)
+			sum = 0;
+			isVisited = new boolean[4][4];
+			s = "";
+			
+			nr1 = shark_r + drS[t.first];
+			nc1 = shark_c + dcS[t.first];
+			
+			if(nr1 < 0 || nc1 < 0 || nr1 >= 4 || nc1 >= 4) continue;
+			sum += infoMap[nr1][nc1].cnt;
+			isVisited[nr1][nc1] = true;
+			
+			nr2 =  nr1 + drS[t.second];
+			nc2 =  nc1 + dcS[t.second];
+			
+			if(nr2 < 0 || nc2 < 0 || nr2 >= 4 || nc2 >= 4) continue;
+			sum += infoMap[nr2][nc2].cnt;
+			isVisited[nr2][nc2] = true;
+			
+			nr3 = nr2 + drS[t.third];
+			nc3 = nc2 + dcS[t.third];
+			
+			if(nr3 < 0 || nc3 < 0 || nr3 >= 4 || nc3 >= 4) continue;
+			if(!isVisited[nr3][nc3]) {
+				sum += infoMap[nr3][nc3].cnt;
+			}
+			
+			s += t.first;
+			s += t.second;
+			s += t.third;
+			
+			pq.offer(new PQType(sum, s, nr1, nr2, nr3, nc1, nc2, nc3));
+		}
+		
+		PQType t = pq.poll();
+		
+		if(infoMap[t.nr1][t.nc1].cnt > 0) { // 조건문 누락 (디버깅3) **
+			tmpMap = new HashMap<>();
+			infoMap[t.nr1][t.nc1] = new Type(0, tmpMap);
+			smellMap[t.nr1][t.nc1] = 3;
+		}
+		
+		if(infoMap[t.nr2][t.nc2].cnt > 0) {
+			tmpMap = new HashMap<>();
+			infoMap[t.nr2][t.nc2] = new Type(0, tmpMap);
+			smellMap[t.nr2][t.nc2] = 3;
+		}
+		
+		if(infoMap[t.nr3][t.nc3].cnt > 0) {
+			tmpMap = new HashMap<>();
+			infoMap[t.nr3][t.nc3] = new Type(0, tmpMap);
+			smellMap[t.nr3][t.nc3] = 3;
+		}
+		
+		shark_r = t.nr3; // 상어좌표 갱신 안해줌 (디버깅4) **
+		shark_c = t.nc3;
+	}
+	public static void moveFish() {
+		Queue<Type3> q = new LinkedList<>();
+		Type[][] tmpInfoArr = new Type[4][4];
+		
+		for(int i=0; i<4; i++) {
+			for(int j=0; j<4; j++) {
+				Type t = infoMap[i][j];
+				tmpInfoArr[i][j] = new Type(t.cnt, t.fishMap);
+			}
+		}
+		
+		int nr, nc, nd, count;
+		boolean moveChk;
+		for(int r=0; r<4; r++) {
+			for(int c=0; c<4; c++) {
+				Type t = infoMap[r][c];
+				moveChk = false; // 물고기가 움직이지 않았을 경우 체크 안해줌 (디버깅5) **
+				
+				for(Integer d : t.fishMap.keySet()) {
+					count = t.fishMap.get(d);
 					
-					if(t.cnt == 0) {
-						smellChkMap[i][j] = null;
-					} else {
-						smellChkMap[i][j] = t;
+					for(int i=0; i<8; i++) {
+						nd = (d - i + 8) % 8;
+						nr = r + dr[nd];
+						nc = c + dc[nd];
+						
+						if(nr < 0 || nc < 0 || nr >= 4 || nc >= 4) continue;
+						if(smellMap[nr][nc] > 0) continue;
+						if(nr == shark_r && nc == shark_c) continue;
+						
+						tmpMap = new HashMap<>();
+						tmpMap.put(nd, count);
+						q.offer(new Type3(nr, nc, count, tmpMap));
+						moveChk = true; // 이동함
+						
+						break;
 					}
 				}
+				
+				if(moveChk) {
+					tmpMap = new HashMap<>();
+					tmpInfoArr[r][c] = new Type(0, tmpMap);
+				}
+				/*
+				if(!moveChk) { // 이동하지 않은 물고기들 저장
+					q.offer(new Type3(r, c, t.cnt, t.fishMap));
+				}*/
 			}
+		}
+		
+		while(!q.isEmpty()) {
+			Type3 t = q.poll();
+			Type originT = tmpInfoArr[t.r][t.c];
+			
+			originT.cnt += t.cnt;
+			for(Integer key : t.fishMap.keySet()) {
+				originT.fishMap.put(key, originT.fishMap.getOrDefault(key, 0) + t.fishMap.get(key));
+			}
+		}
+		
+		for(int i=0; i<4; i++) {
+			for(int j=0; j<4; j++) {
+				Type t = tmpInfoArr[i][j];
+				infoMap[i][j] = new Type(t.cnt, t.fishMap);
+			}
+		}
+	}
+	public static void permutation(int[] arr, int[] tempArr, int depth) {
+		if(depth == 3) {
+			Type2 t = new Type2(tempArr[0], tempArr[1], tempArr[2]);
+			sharkMoveList.add(t);
+			return;
+		}
+		
+		for(int i=0; i<4; i++) {
+			tempArr[depth] = arr[i];
+			permutation(arr, tempArr, depth+1);
 		}
 	}
 }
